d0c74a692393ccdee9a40da0475ad87ce4454a01
ryuseikurata <amnosmusic13@gmail.com>
2020-09-28 01:02:51 +0900

    fix: snake_case

--- functions/src/routes/https/slack/post.ts
+++ functions/src/routes/https/slack/post.ts
@@ -1,5 +1,5 @@
 import * as functions from "firebase-functions";
 
-export const onRequest = functions.https.onRequest(async (req, res) => {
+export const on_request = functions.https.onRequest(async (req, res) => {
 	res.sendStatus(200);
 });
