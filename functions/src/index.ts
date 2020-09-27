22ef170b31b8c5ba7bec4a0d3c7f9e9f8d593026
ryuseikurata <amnosmusic13@gmail.com>
2020-09-28 01:06:35 +0900

    delete comment out

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,9 +7,6 @@ import { config } from "./config";
 
 
 admin.initializeApp({
-    /*credential: admin.credential.cert(
-      JSON.parse(JSON.stringify(service_account))
-    ),*/
     credential: admin.credential.cert(
       JSON.parse(JSON.stringify(config.service_account).replace(/\\\\n/g, "\\n"))
     ),
